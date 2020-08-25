36babf0ddd6352d15adbd555485a9bae310f4038
michaellindberg <michaellindberg@live.com>
2020-08-25 22:19:06 +0200

    changed connection string

--- RegistryManager/DeviceTwins/Program.cs
+++ RegistryManager/DeviceTwins/Program.cs
@@ -8,7 +8,7 @@ namespace DeviceTwins
     {
         static void Main(string[] args)
         {
-            RegistryManager registryManager = RegistryManager.CreateFromConnectionString("HostName=ambiantgroup-iothub-dev.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=s+ShoUvRo+GYGFKqEwI4KG8+TNQ+T2VHnXYkWqfF8Xw=");
+            RegistryManager registryManager = RegistryManager.CreateFromConnectionString("<connection string>");
 
             var query = registryManager.CreateQuery("SELECT * FROM devices");
             var result = query.GetNextAsTwinAsync().Result;
