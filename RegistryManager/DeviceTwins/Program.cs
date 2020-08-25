3cceb4a6ed618a91e78037d74caba1a706c70b3e
michaellindberg <michaellindberg@live.com>
2020-08-25 12:51:34 +0200

    Checked in RegistryManager DeviceTwins sample

---
+++ RegistryManager/DeviceTwins/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Azure.Devices;
+using Newtonsoft.Json;
+
+namespace DeviceTwins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegistryManager registryManager = RegistryManager.CreateFromConnectionString("<connection string>");
+
+            var query = registryManager.CreateQuery("SELECT * FROM devices");
+            var result = query.GetNextAsTwinAsync().Result;
+
+            Console.WriteLine(JsonConvert.SerializeObject(result));
+        }
+    }
+}
